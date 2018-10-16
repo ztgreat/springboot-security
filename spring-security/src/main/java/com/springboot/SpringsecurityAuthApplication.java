f18adf773f6399a4f580343c5f9c4afdd8be8100
小张 <1170881778@qq.com>
2018-10-16 17:45:41 +0800

    init

---
+++ spring-security/src/main/java/com/springboot/SpringsecurityAuthApplication.java
@@ -0,0 +1,12 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityAuthApplication.class, args);
+	}
+}
