a18de6307f7d65b7afcdc8b6c8f1705919bfea7f
小张 <1170881778@qq.com>
2018-11-09 19:35:10 +0800

    init oauth2 module

---
+++ oauth2-client/src/main/java/com/springboot/security/oauth2/config/SecurityConfiguration.java
@@ -0,0 +1,62 @@
+package com.springboot.security.oauth2.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService(){
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+//         password：支持多种编码，通过密码的前缀区分编码方式
+        String finalPassword = "{bcrypt}"+bCryptPasswordEncoder.encode("123456");
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user_1").password(finalPassword).authorities("USER").build());
+        manager.createUser(User.withUsername("user_2").password(finalPassword).authorities("USER").build());
+        return manager;
+    }
+
+
+    // password：支持多种编码，通过密码的前缀区分编码方式,推荐
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+//
+//    /**
+//     * 这一步的配置是必不可少的，否则SpringBoot会自动配置一个AuthenticationManager,覆盖掉内存中的用户
+//     */
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        AuthenticationManager manager = super.authenticationManagerBean();
+        return manager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            .requestMatchers().anyRequest()
+            .and()
+                .authorizeRequests()
+                .antMatchers("/oauth/**").permitAll();
+        // @formatter:on
+    }
+}
